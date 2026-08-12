1aca2a47eb49e9c6e5ded91dec2e8ef1918d115b
Sahil Nadaf <sahilnadafexcelligents@gmail.com>
2026-08-12 21:20:49 +0530

    Arrays

--- 01-Java-Basics/08-Arrays/Arrays.java
+++ 01-Java-Basics/08-Arrays/Arrays.java
@@ -2,6 +2,11 @@ public class Arrays {
     public static void main(String[] args) {
         int[] numbers = new int[5];
         numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+       // numbers[5] = 60; // This line will cause an ArrayIndexOutOfBoundsException
 
         for (int num : numbers) {
             System.out.println(num);
