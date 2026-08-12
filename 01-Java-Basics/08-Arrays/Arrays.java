f2331c90ac0ba0b66a2478fc9cda980c73341f17
Sahil Nadaf <sahilnadafexcelligents@gmail.com>
2026-08-12 21:51:12 +0530

    Array

--- 01-Java-Basics/08-Arrays/Arrays.java
+++ 01-Java-Basics/08-Arrays/Arrays.java
@@ -1,15 +1,15 @@
 public class Arrays {
     public static void main(String[] args) {
-        int[] numbers = new int[5];
-        numbers[0] = 10;
-        numbers[1] = 20;
-        numbers[2] = 30;
-        numbers[3] = 40;
-        numbers[4] = 50;
-       // numbers[5] = 60; // This line will cause an ArrayIndexOutOfBoundsException
+        int[] Arr = { 10, 20, 30, 40, 50 };
+        int sum = 0;
+        int n;
 
-        for (int num : numbers) {
-            System.out.println(num);
+        for (int i = 1; i < Arr.length; i++) {
+            int Value = Arr[i];
+            sum = sum + Value;
+            // If We Sout Here Always It Will Loop Here
         }
+
+        System.out.println(sum); // Here It Dos not loop Here
     }
 }
