863ad257126ef74027c68eb54d93b6879e805f47
Sahil Nadaf <sahilnadafexcelligents@gmail.com>
2026-08-13 00:54:46 +0530

    Joining Of To String In One Line

---
+++ 01-Java-Basics/09-Strings/Concatenation.java
@@ -0,0 +1,10 @@
+public class Concatenation {
+    public static void main(String[] args) {
+        String firstname = "Sahil";
+        String lastname = "Nadaf";
+
+        String fullname = firstname + " " + lastname;
+        System.out.println("My Name Is : " + fullname);
+        // Concatenation Means Joining Of To String In One Line 
+    }
+}
