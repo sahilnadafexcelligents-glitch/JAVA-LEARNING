5b3524300fc53b26683d4da8270018693ef4e223
Sahil Nadaf <sahilnadafexcelligents@gmail.com>
2026-08-13 00:45:35 +0530

    String Demo

---
+++ 01-Java-Basics/09-Strings/StringDemo.java
@@ -0,0 +1,11 @@
+public class StringDemo {
+    public static void main(String[] args) {
+        String greeting = "Hello world";
+        System.out.println(greeting);
+        System.out.println(greeting.length());
+        System.out.println(greeting.toUpperCase());
+        System.out.println(greeting.toLowerCase());
+        System.out.println(greeting.charAt(0));
+        System.out.println(greeting.contains(greeting));
+    }
+}
